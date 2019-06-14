f06d120fb3df1d5dbaca64571af3fd24ac1d975d
4000D <dkdkajej@gmail.com>
2019-06-14 19:01:09 +0900

    contracts: migrate openzeppelin-solidity contracts

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -84,6 +84,62 @@ contract Presale is CappedCrowdsale, AllowanceCrowdsale, WhitelistCrowdsale, Fin
     }
 
     /**
+     * @dev low level token purchase ***DO NOT OVERRIDE***
+     * This function has a non-reentrancy guard, so it shouldn't be called by
+     * another `nonReentrant` function.
+     * @param beneficiary Recipient of the token purchase
+     */
+    function buyTokens(address beneficiary) public nonReentrant payable {
+        uint256 weiAmount = msg.value;
+        _preValidatePurchase(beneficiary, weiAmount);
+
+        // adjust weiAmount with respect to cap and individual cap.
+        weiAmount = _updatePurchaseAmount(beneficiary, weiAmount);
+
+        // calculate token amount to be created
+        uint256 tokens = _getTokenAmount(weiAmount);
+
+        // update state
+        _weiRaised = _weiRaised.add(weiAmount);
+
+        _processPurchase(beneficiary, tokens);
+        emit TokensPurchased(msg.sender, beneficiary, weiAmount, tokens);
+
+        _updatePurchasingState(beneficiary, weiAmount);
+
+        _forwardFunds();
+        _postValidatePurchase(beneficiary, weiAmount);
+    }
+
+    function _updatePurchaseAmount(address beneficiary, uint weiAmount) internal returns (uint) {
+        uint amount = weiAmount;
+        uint maxCap = cap();
+
+        // check max cap
+        uint amountWithWeiRaised = weiRaised().add(amount);
+
+        if (amountWithWeiRaised > maxCap) {
+            amount = amount.sub(amountWithWeiRaised.sub(maxCap));
+        }
+
+        // check individual max cap
+        uint amountWithContribution = contributions[beneficiary].add(amount);
+
+        if (amountWithContribution > individualMaxCap) {
+            amount = amount.sub(amountWithContribution.sub(individualMaxCap));
+        }
+
+        uint refundAmount = weiAmount.sub(amount);
+        if (refundAmount > 0) {
+            // buyTokens is safe for reenterancy attack, so we can transfer transfer here.
+            msg.sender.transfer(refundAmount);
+        }
+
+        return amount;
+    }
+
+
+    /**
      * @dev Extend parent behavior requiring purchase to respect the beneficiary's funding cap.
      * @param beneficiary Token purchaser
      * @param weiAmount Amount of wei contributed
