3d54ed8dbb331053fb754bc2df9880bf93a84adc
Geonwoo, Shin <dn2757@gmail.com>
2019-06-23 16:48:32 +0900

    test: add test cases

--- test/TokenVestingCrowdsale.test.js
+++ test/TokenVestingCrowdsale.test.js
@@ -6,11 +6,17 @@ const TokenVestingCrowdsaleImpl = artifacts.require('TokenVestingCrowdsaleImpl')
 require('chai')
   .should();
 
-contract('TokenVestingCrowdsale', function ([_, owner, wallet, investor]) {
+contract('TokenVestingCrowdsale', function ([_, owner, wallet, beneficiary]) {
   const rate = new BN('2');
-  const value = ether('10');
-  const expectedTokenAmount = rate.mul(value);
-  const saleAmount = new BN('10').pow(new BN('22'));
+  const value = ether('1');
+  const amount = rate.mul(value);
+  const saleAmount = new BN('10').pow(new BN('20'));
+
+  beforeEach(async function () {
+    this.start = (await time.latest()).add(time.duration.minutes(1));
+    this.cliffDuration = time.duration.years(1);
+    this.duration = time.duration.years(2);
+  });
 
   context('once deployed', function () {
     beforeEach(async function () {
@@ -25,23 +31,19 @@ contract('TokenVestingCrowdsale', function ([_, owner, wallet, investor]) {
     it('can buy tokens', async function () {
       await this.token.mint(this.crowdsale.address, saleAmount, { from: owner });
 
-      const { logs } = await this.crowdsale.sendTransaction({ value: value, from: investor });
+      const { logs } = await this.crowdsale.sendTransaction({ value: value, from: beneficiary });
       expectEvent.inLogs(logs, 'TokensPurchased', {
-        purchaser: investor,
-        beneficiary: investor,
+        purchaser: beneficiary,
+        beneficiary: beneficiary,
         value: value,
-        amount: expectedTokenAmount,
+        amount: amount,
       });
       (await this.token.balanceOf(this.crowdsale.address)).should.be.bignumber
-        .equal(saleAmount.sub(expectedTokenAmount));
+        .equal(saleAmount.sub(amount));
     });
 
     it('reverts when all token have not yet sold', async function () {
       await this.token.mint(this.crowdsale.address, saleAmount, { from: owner });
-
-      this.start = (await time.latest()).add(time.duration.minutes(1));
-      this.cliffDuration = time.duration.years(1);
-      this.duration = time.duration.years(2);
       await expectRevert(
         this.crowdsale._initiate(this.start, this.cliffDuration, this.duration),
         'TokenVestingCrowdsale: all tokens have not been sold yet'
@@ -49,14 +51,53 @@ contract('TokenVestingCrowdsale', function ([_, owner, wallet, investor]) {
     });
 
     it('reverts when initiation already happened', async function () {
-      this.start = (await time.latest()).add(time.duration.minutes(1));
-      this.cliffDuration = time.duration.years(1);
-      this.duration = time.duration.years(2);
       await this.crowdsale._initiate(this.start, this.cliffDuration, this.duration);
       await expectRevert(
         this.crowdsale._initiate(this.start, this.cliffDuration, this.duration),
         'TokenVestingCrowdsale: already initiated'
       );
+    });
+
+    it('can not be released before initiation', async function () {
+      await expectRevert(
+        this.crowdsale._release(beneficiary, { from: beneficiary }),
+        'TokenVesting: no tokens are due'
+      );
+    });
+
+    context('after initiated', function () {
+      beforeEach(async function () {
+        await this.token.mint(this.crowdsale.address, saleAmount, { from: owner });
+
+        await this.crowdsale.sendTransaction({ value: value, from: beneficiary });
+
+        const remainingAmount = await this.token.balanceOf(this.crowdsale.address);
+        await this.crowdsale.sendTransaction({ value: remainingAmount.div(rate) });
+
+        await this.crowdsale._initiate(this.start, this.cliffDuration, this.duration);
+      });
+
+      it('cannot be released before cliff', async function () {
+        await expectRevert(this.crowdsale._release(beneficiary),
+          'TokenVesting: no tokens are due'
+        );
+      });
+
+      it('should release proper amount after cliff', async function () {
+        await time.increaseTo(this.start.add(this.cliffDuration));
+
+        await this.crowdsale._release(beneficiary);
+        const releaseTime = await time.latest();
+
+        const releasedAmount = amount.mul(releaseTime.sub(this.start)).div(this.duration);
+        (await this.token.balanceOf(beneficiary)).should.bignumber.equal(releasedAmount);
+      });
+
+      it('should have released all after end', async function () {
+        await time.increaseTo(this.start.add(this.duration));
+        await this.crowdsale._release(beneficiary);
+        (await this.token.balanceOf(beneficiary)).should.bignumber.equal(amount);
+      });
     });
   });
 });
