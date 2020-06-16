0d95faccb95391ba5ef70b7613105ef920521f35
thomashin <dn2757@gmail.com>
2020-06-16 15:20:09 +0900

    re-deploy private ton contract

---
+++ migrations/6_deploy_private_ton.js
@@ -0,0 +1,28 @@
+const VestingToken = artifacts.require('VestingToken');
+const holders = require('../private_ton_holders.js');
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+module.exports = async function (deployer) {
+  if (!process.env.PRIVATESALE) return;
+
+  let token;
+
+  deployer.deploy(VestingToken,
+    ZERO_ADDRESS,
+    ZERO_ADDRESS,
+    0,
+    'Privatesale Tokamak Network Token',
+    18,
+    'PrivateTON',
+    true,
+  ).then(async () => { token = await VestingToken.deployed(); })
+    .then(async () => {
+      const txs = holders.map(holder => token.generateTokens(holder.address, holder.amount));
+      await Promise.all(txs);
+    })
+    .catch((e) => {
+      console.error(e);
+      throw e;
+    });
+};
---
+++ private_ton_holders.js
@@ -0,0 +1,113 @@
+const { BN, toWei } = require('web3-utils');
+const ether = n => new BN(toWei(n, 'ether'));
+
+module.exports = [
+  {
+    address: '0x05dbad8b98fd883c3313bb49daff4cdd61b55b02',
+    amount: ether('800'),
+  },
+  {
+    address: '0x1c0a5dbec8a67490013e96edbcc18facd90dcda7',
+    amount: ether('9024'),
+  },
+  {
+    address: '0x1e648b44697351a3dcbde89d41bc21e3497aa04e',
+    amount: ether('1520'),
+  },
+  {
+    address: '0x27385d2a90f14b1309a2ea1b7673832978044c6e',
+    amount: ether('4000.000003'),
+  },
+  {
+    address: '0x2818824255db21f1451d359d1eee282b3d1818d9',
+    amount: ether('8000'),
+  },
+  {
+    address: '0x32c5cc35c5696953162004b666712d28bf89a125',
+    amount: ether('10789.3952'),
+  },
+  {
+    address: '0x3d827286780dbc00ace4ee416ad8a4c5daac972c',
+    amount: ether('536.0000034'),
+  },
+  {
+    address: '0x3f17edc8cf0eb20f011c7ece6c1c4c1fc84ecf00',
+    amount: ether('7520'),
+  },
+  {
+    address: '0x4291e67a9746dc7805622a10176635d3790acdd1',
+    amount: ether('352.0000022'),
+  },
+  {
+    address: '0x443a8e36753ccd89108f1b0fc92f24eea7c7fdbc',
+    amount: ether('12000'),
+  },
+  {
+    address: '0x5655d5288ec8f70b20c018019e9d12cb4bb15b79',
+    amount: ether('8000.000007'),
+  },
+  {
+    address: '0x56df45d0c4ae9d11ad48a51921398675ca82e355',
+    amount: ether('1600'),
+  },
+  {
+    address: '0x5b2373710f9223b6db69eb934ea1e90cc41cf650',
+    amount: ether('536.0000034'),
+  },
+  {
+    address: '0x5d76ea2fca6e00cff91588eabf4de37bafd73469',
+    amount: ether('7520'),
+  },
+  {
+    address: '0x6765715ed59ccba57204704cdb0a442f7f643041',
+    amount: ether('4799.959648'),
+  },
+  {
+    address: '0x6855b8ecf02f27ecdeff72f409c9ccb631009cb8',
+    amount: ether('352.0000022'),
+  },
+  {
+    address: '0x6a4b25c58173976c4a2bd2151028c3759a1eadd3',
+    amount: ether('352.0000022'),
+  },
+  {
+    address: '0x72ccc4bede1a7d09aa20d97355543d0640c661f1',
+    amount: ether('5146.688'),
+  },
+  {
+    address: '0x7897ccd146b97639c0dd99a17383e0b11681996e',
+    amount: ether('28000'),
+  },
+  {
+    address: '0x85697b276dcd09e3781e1a75b1057e1296773dd9',
+    amount: ether('536.0000034'),
+  },
+  {
+    address: '0x986d9bb496abd602ffbab9291a2621834f0f03a3',
+    amount: ether('536.0000034'),
+  },
+  {
+    address: '0x9f42c2886bf25fe99d5df51ee0a653903c92131f',
+    amount: ether('11200.04035'),
+  },
+  {
+    address: '0xa3a3f93de2ec648b116e07570a89eeeb4bf84100',
+    amount: ether('4000'),
+  },
+  {
+    address: '0xab43dc55e52c97069ae6e234233583ea5108c45e',
+    amount: ether('4800'),
+  },
+  {
+    address: '0xba928d5abd577e3024880b1f00fca3033bca89c4',
+    amount: ether('2480'),
+  },
+  {
+    address: '0xe300eda0ed3e6d1c89b1568d47b8d5377fd21327',
+    amount: ether('8000'),
+  },
+  {
+    address: '0xea5d97a91b10a42e9cc1ac0a5b01b0718999d4a4',
+    amount: ether('1600'),
+  },
+];
