0f5d4eecbf4f723462c4a54cc7f8dff1d5478c6c
thomashin <dn2757@gmail.com>
2020-06-16 15:35:59 +0900

    migrations: check the number of holders

--- migrations/6_deploy_private_ton.js
+++ migrations/6_deploy_private_ton.js
@@ -5,6 +5,7 @@ const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 module.exports = async function (deployer) {
   if (!process.env.PRIVATESALE) return;
+  if (holders.length !== 27) return;
 
   let token;
 
