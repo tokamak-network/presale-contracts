e52985734b5fae77fdecc3622461186ac7ce013f
modagi <modagi@gmail.com>
2020-06-25 07:15:07 +0000

    migration: added one more token

---
+++ migrations/8_deploy_ton.js
@@ -0,0 +1,24 @@
+const TON = artifacts.require('TON');
+const fs = require('fs');
+const { BN, constants, ether } = require('openzeppelin-test-helpers');
+const accounts = require('../test_accounts.json');
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+const wallet = '0xf35A0c48c970d5abFBC1B33096A83bFc87A4a82E';
+const decimal = new BN('18');
+const totalSupply = ether('224000.1');
+
+
+module.exports = async function (deployer) {
+  if (process.env.DAEMONTEST) {
+    let token;
+    await deployer.deploy(TON).then(async () => { token = await TON.deployed(); })
+    .then(() => token.mint(accounts['owner'], ether('10000000')));
+    let data = JSON.parse(fs.readFileSync('deployed.json').toString());
+    data['TON'] = (await TON.deployed()).address
+    fs.writeFile('deployed.json', JSON.stringify(data), (err) => {
+      if (err) throw err;
+    });
+  }
+};
