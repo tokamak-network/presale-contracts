2846df1c06ad99f50263278c1bf7e342f9aec592
modagi <modagi@gmail.com>
2020-06-25 07:08:19 +0000

    config: added rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const fs = require('fs');
 const PrivateKeyProvider = require('truffle-privatekey-provider');
+const pk = fs.readFileSync(".pk").toString().trim();
 
 module.exports = {
   networks: {
@@ -26,6 +28,13 @@ module.exports = {
       port: 8555,
       gas: 0xfffffffffff,
       gasPrice: 0x01,
+    },
+    rinkeby: {
+      provider: new PrivateKeyProvider(pk, "https://rinkeby.infura.io/v3/a608d5cd0f1e42109d964abdffe5d8d9"),
+      network_id: '4', // eslint-disable-line camelcase
+      gas: 5000000,
+      gasPrice: 20000000000,
+      skipDryRun: true,
     },
   },
   mocha: {
